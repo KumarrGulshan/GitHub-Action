ca28806cb01088ae7860a24cf844d5808656709f
Gulshan kumar <ronyahuja806@gmail.com>
2025-10-25 12:39:45 +0530

    first commit

---
+++ src/main/java/Action/Github/ci/cd/action/Application.java
@@ -0,0 +1,26 @@
+package Action.Github.ci.cd.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+	@GetMapping("/Gulshan")
+	public String welcome(){
+		 return "welcome to my world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/KumarrGulshan/GitHub-Action.git
+//git push -u origin main
